65039a8cf4f39a6a38f5bcb1df5ef5bbd0908377
saikumarshanigarapu <saikumarshanigarapu1919@gmail.com>
2025-07-23 02:37:48 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+document.querySelector('.order-btn').addEventListener('click', () => {
+  alert('Order feature coming soon!');
+});
+
+document.querySelector('.contact-btn').addEventListener('click', () => {
+  alert('Redirecting to contact form...');
+});
